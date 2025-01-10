f6541fa5f0a77d8d11b1730d17380d193534bf39
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-01-10 21:39:50 +0000

    dashborad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'connexion', pathMatch: 'full'},
   {
     path: 'connexion',
     loadComponent: () => import('./authentication/login/login.component').then(c => c.LoginComponent)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./layout/layout.component').then(c => c.LayoutComponent),
+    children: [
+      {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+      {path: 'dashboard', loadComponent: () => import('./home/dashboard/dashboard.component').then(c => c.DashboardComponent)},
+    ]
+
   },
 
 ];
