08b32d365606e11e787141758a6947a9efb1ac62
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-01-13 23:44:15 +0000

    article et dash

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
+import {ArticleComponent} from "./article/article.component";
+import {Dashboard} from "./dashboard/dashboard";
 
 
 export default [
 
+    { path: '', redirectTo:'dash', pathMatch: 'full' },
+    { path: 'dash', component:Dashboard, title: 'dash' },
+    { path: 'article', component:ArticleComponent, title: 'Article' },
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
