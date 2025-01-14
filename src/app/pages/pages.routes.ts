4254cb7f497ae15a63d12907a7e633e3017db8f3
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-01-14 13:50:12 +0000

    add projet to route

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -3,6 +3,7 @@ import {ArticleComponent} from "./article/article.component";
 import {Dashboard} from "./dashboard/dashboard";
 import {ClientComponent} from "./client/client.component";
 import {UserComponent} from "./user/user.component";
+import {ProjetComponent} from "./projet/projet.component";
 
 
 export default [
@@ -10,6 +11,7 @@ export default [
     { path: '', redirectTo:'dash', pathMatch: 'full' },
     { path: 'dash', component:Dashboard, title: 'Dash' },
     { path: 'user', component:UserComponent, title: 'Utilisateur' },
+    { path: 'projet', component:ProjetComponent, title: 'Projet' },
     { path: 'client', component:ClientComponent, title: 'Client' },
     { path: 'article', component:ArticleComponent, title: 'Article' },
     { path: '**', redirectTo: '/notfound' }
