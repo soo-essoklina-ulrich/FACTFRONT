651ae4f693b4251acfb6a8da335fcad126cd2779
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-01-14 12:23:09 +0000

    cleint ok

--- src/app/pages/client/client.component.ts
+++ src/app/pages/client/client.component.ts
@@ -9,6 +9,11 @@ import {Client} from "../../models/Client";
 import {ClientService} from "../../services/client/client.service";
 import {InputText} from "primeng/inputtext";
 import {Tag} from "primeng/tag";
+import {Dialog} from "primeng/dialog";
+import {FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {Checkbox} from "primeng/checkbox";
+import {Password} from "primeng/password";
+import {InputNumber} from "primeng/inputnumber";
 
 @Component({
     selector: 'app-client',
@@ -19,7 +24,13 @@ import {Tag} from "primeng/tag";
         Button,
         Tooltip,
         InputText,
-        Tag
+        Tag,
+        Dialog,
+        Checkbox,
+        FormsModule,
+        Password,
+        ReactiveFormsModule,
+        InputNumber
     ],
     templateUrl: './client.component.html',
     styleUrl: './client.component.scss',
@@ -33,6 +44,12 @@ export class ClientComponent implements OnInit {
     clientso: Client[] = [];
     client!: Client;
 
+    visibleaddmoal: boolean = false;
+    visibleeditmoal: boolean = false;
+    dialogheader!: string;
+
+    form!: FormGroup;
+
     constructor(
         private ClientService: ClientService,
         private messageService: MessageService
@@ -40,15 +57,40 @@ export class ClientComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.createform();
         this.getClients();
     }
 
-    filterGlobal(event: Event) {
+    private createform() {
+        this.form = this.ClientService.crateForm();
+    }
 
+    filterGlobal(event: Event) {
+        const value = (event.target as HTMLInputElement).value;
+        if (value.length>0){
+            this.clients = this.clients.filter((client) => client.nom.toLowerCase().includes(value.toLowerCase())|| client.sigle.toLowerCase().includes(value.toLowerCase()));
+        }else {
+            this.clients = this.clientso;
+        }
     }
 
     showAddClientModal() {
+        this.dialogheader = 'Ajout Client';
+        this.visibleaddmoal = true;
+    }
 
+    saveClient() {
+        this.ClientService.saveClient(this.form.value).subscribe(
+            (data) => {
+                this.clientso = this.clients = [...this.clients, data];
+                this.messageService.add({severity: 'success', summary: 'Success', detail: 'Client added successfully'});
+                this.visibleaddmoal = false;
+            },
+            (error) => {
+                console.log(error);
+                this.messageService.add({severity: 'error', summary: 'Error', detail: 'Error'});
+            }
+        )
     }
 
     getClients() {
@@ -56,7 +98,7 @@ export class ClientComponent implements OnInit {
         setTimeout(() => {
             this.ClientService.getClients().subscribe(
                 (data) => {
-                    this.clientso = this.clients = data;
+                    this.clientso = this.clients = data.sort((a, b) => a.nom.localeCompare(b.nom));
                     this.loading = false;
                 },
                 (error) => {
@@ -67,16 +109,23 @@ export class ClientComponent implements OnInit {
         }, 2000)
     }
 
-    editClient(id:string) {
-
+    editClient(id: string) {
+        this.dialogheader = 'Edit Client';
+        this.client = this.clients.find((client) => client.id === id) as Client;
+        this.form.patchValue(this.client);
+        this.visibleeditmoal = true;
     }
 
-    deleteClient(id:string) {
+    deleteClient(id: string) {
         setTimeout(() => {
             this.ClientService.deleteClient(id).subscribe(
                 (data) => {
                     this.clientso = this.clients = this.clients.filter((client) => client.id !== id);
-                    this.messageService.add({severity: 'success', summary: 'Success', detail: 'Client deleted successfully'});
+                    this.messageService.add({
+                        severity: 'success',
+                        summary: 'Success',
+                        detail: 'Client deleted successfully'
+                    });
                 },
                 (error) => {
                     this.messageService.add({severity: 'error', summary: 'Error', detail: 'Error'});
