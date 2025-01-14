f125a5cd79d7a31ca4e3a7bd5e1e91fe325c5eac
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-01-14 11:04:35 +0000

    make guard to home route and child

--- src/app/services/guard/homeguard.guard.ts
+++ src/app/services/guard/homeguard.guard.ts
@@ -4,6 +4,5 @@ import {JwtdecodeService} from "../decode/jwtdecode.service";
 
 
 export const homeguardGuard: CanActivateFn = (route, state) => {
-    console.log('homeguardGuard', inject(JwtdecodeService).isTokenExpired())
     return  inject(JwtdecodeService).isTokenExpired();
 };
