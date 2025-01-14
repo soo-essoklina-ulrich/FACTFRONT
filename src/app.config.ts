7e4be2dcf1e655d2d4ee689a577d14337dbbf198
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-01-14 00:32:13 +0000

    loader dynamic dialogue article

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,16 +1,23 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
+import {setjwtInterceptor} from "./app/services/interceptor/setjwt.interceptor";
+import {jwtErrorInterceptor} from "./app/services/interceptor/jwt-error.interceptor";
+import {DialogService} from "primeng/dynamicdialog";
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
-        provideHttpClient(withFetch()),
+        provideHttpClient(withFetch(), withInterceptors([
+            setjwtInterceptor,
+            jwtErrorInterceptor
+        ])),
         provideAnimationsAsync(),
-        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
+        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
+        DialogService
     ]
 };
--- src/app/pages/article/article.component.ts
+++ src/app/pages/article/article.component.ts
@@ -1,11 +1,155 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {Article} from "../../models/Article";
+import {ArticleService} from "../../services/article/article.service";
+import {MessageService} from "primeng/api";
+import {TableModule} from "primeng/table";
+import {Button} from "primeng/button";
+import {Tooltip} from "primeng/tooltip";
+import {Dialog} from "primeng/dialog";
+import {Toast} from "primeng/toast";
+import {NgIf} from "@angular/common";
+import {InputText} from "primeng/inputtext";
+import {LoaderComponent} from "../../layout/component/loader/loader.component";
 
 @Component({
-  selector: 'app-article',
-  imports: [],
-  templateUrl: './article.component.html',
-  styleUrl: './article.component.scss'
+    selector: 'app-article',
+    imports: [
+        TableModule,
+        Button,
+        Tooltip,
+        Dialog,
+        ReactiveFormsModule,
+        Toast,
+        NgIf,
+        InputText,
+        LoaderComponent
+    ],
+    templateUrl: './article.component.html',
+    styleUrl: './article.component.scss',
+    providers: [MessageService]
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
+
+    visibleaddmoal: boolean = false;
+
+    protected form!: FormGroup;
+    protected articlesListo: Article[] = [];
+    protected articlesList: Article[] = [];
+    protected article!: Article;
+    loading: boolean = true;
+    protected visibleeditmoal: boolean = false;
+
+
+    constructor(
+        private ArticleService: ArticleService,
+        private messageService: MessageService
+    ) {
+    }
+
+    ngOnInit(): void {
+        this.createform()
+        this.getArticles();
+    }
+
+
+    createform() {
+        this.form = this.ArticleService.createform();
+    }
+
+    getArticles() {
+        this.loading = true;
+        setTimeout(() => {
+            this.ArticleService.getArticles().subscribe(
+                (res) => {
+                    this.articlesListo = this.articlesList = res;
+                    this.loading = false;
+                    this.messageService.add({
+                        severity: 'success',
+                        summary: 'Success',
+                        detail: 'Articles Charge successfully'
+                    });
+                },
+                (err) => {
+                    console.log(err);
+                    this.messageService.add({severity: 'error', summary: 'Error', detail: err.error.detail});
+                }
+            );
+        }, 3000)
+    }
+
+    OnSubmit() {
+        this.ArticleService.addArticle(this.form.value).subscribe(
+            (res) => {
+                this.articlesList.push(res);
+                this.articlesListo = this.articlesList;
+                this.visibleaddmoal = false;
+                this.form.reset();
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Success',
+                    detail: 'Article added successfully'
+                });
+            },
+            (err) => {
+                this.messageService.add({severity: 'error', summary: 'Error', detail: err.error.detail});
+            }
+        );
+    }
+
+    UpdataeArticle() {
+        this.ArticleService.updateArticle(this.article.id, this.form.value).subscribe(
+            (res) => {
+                this.articlesList = this.articlesList.map((article) => article.id === this.article.id ? res : article);
+                this.articlesListo = this.articlesList;
+                this.visibleeditmoal = false;
+                this.form.reset();
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Success',
+                    detail: 'Article updated successfully'
+                });
+            },
+            (err) => {
+                this.messageService.add({severity: 'error', summary: 'Error', detail: err.error.detail});
+            }
+        );
+    }
+
+    deleteArticle(id: string) {
+        this.ArticleService.deleteArticle(id).subscribe(
+            (res) => {
+                this.articlesList = this.articlesList.filter((article) => article.id !== id);
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Success',
+                    detail: 'Article deleted successfully'
+                });
+            },
+            (err) => {
+                this.messageService.add({severity: 'error', summary: 'Error', detail: err.error.detail});
+            }
+        );
+    }
+
+    filterGlobal($event: Event) {
+        const value = ($event.target as HTMLInputElement).value;
+
+        if (value.length > 0) {
+            this.articlesList = this.articlesList.filter((article) => article.libelle.toLowerCase().includes(value.toLowerCase()));
+        } else {
+            this.articlesList = this.articlesListo;
+        }
+    }
+
+    editArticle(id: string) {
+        this.article = this.articlesList.find((article) => article.id === id)!;
+        this.form.patchValue(this.article);
+        this.visibleeditmoal = true;
+    }
+
+    showAddArticleModal() {
+        this.visibleaddmoal = true;
+    }
 
 }
