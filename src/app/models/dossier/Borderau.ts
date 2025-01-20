0dfbf0fa467e245721df0e00c5dbd79e55fa4753
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-01-20 00:54:51 +0000

    Add Borderau interface for managing Borderau data structure

---
+++ src/app/models/dossier/Borderau.ts
@@ -0,0 +1,13 @@
+import {Article_Quantite} from "./Article_Quantite";
+
+export interface Borderau {
+    id:string,
+    reference:string,
+    numero:string,
+    articleQuantiteslist:Article_Quantite[],
+    total_ht:number,
+    total_ttc: number,
+    total_tva: number,
+    client: string,
+    date: Date
+}
