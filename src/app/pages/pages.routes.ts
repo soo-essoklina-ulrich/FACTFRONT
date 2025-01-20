134081e9cdca518c7fb2b68591b9cd75e2aa2e47
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-01-20 00:18:42 +0000

    Add routes for Proforma, Borderau, Facture,

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,6 +4,9 @@ import {Dashboard} from "./dashboard/dashboard";
 import {ClientComponent} from "./client/client.component";
 import {UserComponent} from "./user/user.component";
 import {ProjetComponent} from "./projet/projet.component";
+import {ProformaComponent} from "./dossier/proforma/proforma.component";
+import {BorderauComponent} from "./dossier/borderau/borderau.component";
+import {FactureComponent} from "./dossier/facture/facture.component";
 
 
 export default [
@@ -12,6 +15,9 @@ export default [
     { path: 'dash', component:Dashboard, title: 'Dash' },
     { path: 'user', component:UserComponent, title: 'Utilisateur' },
     { path: 'projet', component:ProjetComponent, title: 'Projet' },
+    { path: 'proforma', component:ProformaComponent, title: 'Proforma' },
+    { path: 'borderau', component:BorderauComponent, title: 'Borderau' },
+    { path: 'facture', component:FactureComponent, title: 'Facture' },
     { path: 'client', component:ClientComponent, title: 'Client' },
     { path: 'article', component:ArticleComponent, title: 'Article' },
     { path: '**', redirectTo: '/notfound' }
