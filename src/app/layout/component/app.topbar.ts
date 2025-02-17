0ad2dd4ee44b83af109acad7596e40eb8640724d
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-02-17 01:00:26 +0000

    add element signout in topbar

--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -1,69 +1,87 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { StyleClassModule } from 'primeng/styleclass';
 import { AppConfigurator } from './app.configurator';
 import { LayoutService } from '../service/layout.service';
+import { SpeedDial } from 'primeng/speeddial';
+import { LogoutService } from '../../services/auth/logout.service';
+import { Dialog } from 'primeng/dialog';
+import { visible } from 'ansi-colors';
+import { Button } from 'primeng/button';
+import { Tooltip } from 'primeng/tooltip';
 
 @Component({
     selector: 'app-topbar',
     standalone: true,
-    imports: [RouterModule, CommonModule, StyleClassModule, AppConfigurator],
-    template: ` <div class="layout-topbar">
-        <div class="layout-topbar-logo-container">
-            <button class="layout-menu-button layout-topbar-action" (click)="layoutService.onMenuToggle()">
-                <i class="pi pi-bars"></i>
-            </button>
-            <a class="layout-topbar-logo" routerLink="/home">
-               <img src="/identity_redim.ico" width="40">
-                <span>SOOSMART Group</span>
-            </a>
-        </div>
-
-        <div class="layout-topbar-actions">
-            <div class="layout-config-menu">
-                <button type="button" class="layout-topbar-action" (click)="toggleDarkMode()">
-                    <i [ngClass]="{ 'pi ': true, 'pi-moon': layoutService.isDarkTheme(), 'pi-sun': !layoutService.isDarkTheme() }"></i>
+    imports: [RouterModule, CommonModule, StyleClassModule, AppConfigurator, Dialog, Button, Tooltip],
+    template: `
+        <div class="layout-topbar">
+            <div class="layout-topbar-logo-container">
+                <button class="layout-menu-button layout-topbar-action" (click)="layoutService.onMenuToggle()">
+                    <i class="pi pi-bars"></i>
                 </button>
-                <div class="relative">
-                    <button
-                        class="layout-topbar-action layout-topbar-action-highlight"
-                        pStyleClass="@next"
-                        enterFromClass="hidden"
-                        enterActiveClass="animate-scalein"
-                        leaveToClass="hidden"
-                        leaveActiveClass="animate-fadeout"
-                        [hideOnOutsideClick]="true"
-                    >
-                        <i class="pi pi-palette"></i>
-                    </button>
-                    <app-configurator />
-                </div>
+                <a class="layout-topbar-logo" routerLink="/home">
+                    <img src="/identity_redim.ico" width="40">
+                    <span>SOOSMART Group</span>
+                </a>
             </div>
 
-            <button class="layout-topbar-menu-button layout-topbar-action" pStyleClass="@next" enterFromClass="hidden" enterActiveClass="animate-scalein" leaveToClass="hidden" leaveActiveClass="animate-fadeout" [hideOnOutsideClick]="true">
-                <i class="pi pi-ellipsis-v"></i>
-            </button>
-
-            <div class="layout-topbar-menu hidden lg:block">
-                <div class="layout-topbar-menu-content">
-
-                    <button type="button" class="layout-topbar-action" routerLink="/home/profile">
-                        <i class="pi pi-user"></i>
-                        <span>Profile</span>
+            <div class="layout-topbar-actions">
+                <div class="layout-config-menu">
+                    <button type="button" class="layout-topbar-action" (click)="toggleDarkMode()">
+                        <i [ngClass]="{ 'pi ': true, 'pi-moon': layoutService.isDarkTheme(), 'pi-sun': !layoutService.isDarkTheme() }"></i>
                     </button>
+                    <div class="relative">
+                        <button
+                            class="layout-topbar-action layout-topbar-action-highlight"
+                            pStyleClass="@next"
+                            enterFromClass="hidden"
+                            enterActiveClass="animate-scalein"
+                            leaveToClass="hidden"
+                            leaveActiveClass="animate-fadeout"
+                            [hideOnOutsideClick]="true"
+                        >
+                            <i class="pi pi-palette"></i>
+                        </button>
+                        <app-configurator />
+                    </div>
+                </div>
+
+                <button class="layout-topbar-menu-button layout-topbar-action" pStyleClass="@next"
+                        enterFromClass="hidden" enterActiveClass="animate-scalein" leaveToClass="hidden"
+                        leaveActiveClass="animate-fadeout" [hideOnOutsideClick]="true">
+                    <i class="pi pi-ellipsis-v"></i>
+                </button>
+
+                <div class="layout-topbar-menu hidden lg:block">
+                    <div class="layout-topbar-menu-content">
+                        <button type="button" class="layout-topbar-action" (click)="visible = true">
+                            <i class="pi pi-sign-out"></i>
+                            <span>Profile</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>`
+        <p-dialog header="Logout" [(visible)]="visible" [modal]="true" [style]="{width: '10vw'}" [baseZIndex]="10000">
+            <div class=" flex justify-around">
+                <p-button type="button" icon="pi pi-times" iconPos="right" severity="info" (click)="visible=false" pTooltip=" Annuler" />
+                <p-button type="button" icon="pi pi-sign-out" iconPos="right" severity="danger" pTooltip="Logout"
+                          (click)="logoutService.logout()" />
+            </div>
+        </p-dialog>
+    `
 })
 export class AppTopbar {
     items!: MenuItem[];
 
-    constructor(public layoutService: LayoutService) {}
+    constructor(public layoutService: LayoutService, protected logoutService: LogoutService) {}
 
     toggleDarkMode() {
         this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
     }
+
+    protected  visible:boolean = false;
 }
